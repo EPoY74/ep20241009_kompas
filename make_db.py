b190dc5c3c18e69556ac46ceb686b4ba4d56c8dd
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-09 21:55:26 +0500

    Сделал подлючение к БД,

--- make_db.py
+++ make_db.py
@@ -1,5 +1,5 @@
 """
-Создаю БД и таблицы для хранения финансоыой информации
+Создаю БД и таблицы для хранения финансовой информации
 Автор: Евгений Петров
 Почта: p174@mail.ru
 Дата: 09.10.2024
@@ -7,3 +7,17 @@
 
 import psycopg2
 
+
+import settings
+import work_postgresql
+
+
+if __name__ == "__main__":
+    DB_NAME : str = settings.KOMPASS_DBNAME
+    DB_USER : str = settings.KOMPASS_USER_DB
+    DB_PASSWORD : str = settings.KOMPASS_PASSWORD_DB
+  
+    db_connect = work_postgresql.connect_to_db(DB_NAME, DB_USER, DB_PASSWORD)
+    
+    db_connect.close()
+    
