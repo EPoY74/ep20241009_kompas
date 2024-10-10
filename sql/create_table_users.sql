8f3e18c6a4c98c87acb3b05fe9bafef6190b2827
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-10 12:02:36 +0500

    Переименовал файл с sql запросом для создания таблицы users на более корректное название файла

---
+++ sql/create_table_users.sql
@@ -0,0 +1,11 @@
+-- Создает таблицу с пользователями
+    CREATE TABLE IF NOT EXISTS users_compass(
+    client_id SERIAL PRIMARY KEY,
+    first_name VARCHAR,
+    second_name VARCHAR,
+    account_open_day DATE,
+    accouns_close_day DATE,
+    email VARCHAR,
+    phone_number VARCHAR,
+    is_blocked boolean,
+    )
