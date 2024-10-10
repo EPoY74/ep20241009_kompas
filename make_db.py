c2832c22b400869bd9dbe572c19bd85c1cf3a32a
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-10 08:09:29 +0500

    Пока что закоменьтил не используемые импорты

--- make_db.py
+++ make_db.py
@@ -5,21 +5,18 @@
 Дата: 09.10.2024
 """
 
-import psycopg2
+# import psycopg2
+# from psycopg2.extensions import connection
 
 
-import settings
+# import settings
 import work_postgresql
 
 
 def main():
     """Основной код и логика программы.
     """
-    DB_NAME : str = settings.KOMPASS_DBNAME
-    DB_USER : str = settings.KOMPASS_USER_DB
-    DB_PASSWORD : str = settings.KOMPASS_PASSWORD_DB
-
-    db_connect = work_postgresql.connect_to_db(DB_NAME, DB_USER, DB_PASSWORD)
+    db_connect = work_postgresql.conn_to_db()
 
     db_connect.close()
 
