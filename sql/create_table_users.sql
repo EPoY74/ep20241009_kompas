cfd1e8247a76107b9c01c857881feee7f0987aa9
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-11 16:37:11 +0500

    Внес обзательные для заполнения поля

--- sql/create_table_users.sql
+++ sql/create_table_users.sql
@@ -1,11 +1,11 @@
 -- Создает таблицу с пользователями
     CREATE TABLE IF NOT EXISTS users_compass(
     client_id SERIAL PRIMARY KEY,
-    first_name VARCHAR,
+    first_name VARCHAR NOT NULL,
     second_name VARCHAR,
     surname VARCHAR,
-    account_open_day DATE,
+    account_open_day DATE NOT NULL,
     accouns_close_day DATE,
     email VARCHAR,
-    phone_number VARCHAR,
-    is_blocked boolean)
+    phone_number VARCHAR NOT NULL,
+    is_blocked boolean NOT NULL DEFAULT FALSE)
