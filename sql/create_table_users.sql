9acfe5dd5525ae11f9a06224043d648fe0aa6057
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-11 16:34:34 +0500

    Переделал запрос на создание таблцы, добавил поле surname(отчество)

--- sql/create_table_users.sql
+++ sql/create_table_users.sql
@@ -3,6 +3,7 @@
     client_id SERIAL PRIMARY KEY,
     first_name VARCHAR,
     second_name VARCHAR,
+    surname VARCHAR,
     account_open_day DATE,
     accouns_close_day DATE,
     email VARCHAR,
