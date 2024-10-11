9d45a15382ec2951133699eb187a859f4802725f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-11 12:42:37 +0500

    Сделал фукнцию для созданияя таблицы через sql запрос(общая) и сделал функцию для для создания таблицы users

--- create_table.py
+++ create_table.py
@@ -8,8 +8,9 @@ from psycopg2.extensions import connection
 import work_postgresql
 
 
-def create_table_users(inner_db_connect: connection, inner_sql_query: str):
-    """Функция создает таблицу users 
+def create_table(inner_db_connect: connection, inner_sql_query: str):
+    """Функция создает таблицe с заданными параметрами
+    в заданной БД
 
     Args:
         inner_db_connect (connection): соединение с БД, в которой создаем таблицу
@@ -18,8 +19,20 @@ def create_table_users(inner_db_connect: connection, inner_sql_query: str):
     try:
         work_postgresql.write_to_db(inner_db_connect,inner_sql_query)
     except ValueError as err:
-        print(f "Ошибка выполнения:  {err}")
+        print(f"Ошибка выполнения:  {err}")
     else:
         print("SQL запрос для создания таблицы users выполнен успешно")
     finally:
         print("Завершение работы функции создания таблицы users")
+        
+        
+def create_table_users():
+    """Создаю таблицу users.
+    Передача дополнительных переметров не требуется, так как
+    все неодходитом подключается в данной функции.
+    """
+    with open("./sql/create_table_users.sql", "r", encoding="utf-8") as file:
+        sql_query : str = file.read()
+    
+    db_conn: connection = work_postgresql.conn_to_db()
+    create_table(db_conn,sql_query)
