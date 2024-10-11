ea38d92c6dce0e456b0a66c5da2f024e275221f0
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-11 17:32:35 +0500

    sql запрос на создание таблицы с операциями

--- sql/create_table_operations.sql
+++ sql/create_table_operations.sql
@@ -1,1 +1,17 @@
+-- Active: 1728491536018@@127.0.0.1@5432@Kompass-db
 -- создает таблицу операции
+CREATE TABLE IF NOT EXISTS operations_kompass(
+    transaction_ID SERIAL PRIMARY KEY,
+    account_id INTEGER REFERENCES account_kompass(account_id),
+    --сумма операции
+    amount DECIMAL (10,2) NOT NULL DEFAULT 0,
+    --тип операции (депозит, снятие и тд)б из отдельной таблицы
+    operation_type varchar NOT NULL,
+    -- дата время авторизации
+    authorization_day TIMESTAMP NOT NULL
+    --дата время транзакции
+    transaction_date TIMESTAMP,
+    --описание операции
+    operation_description VARCHAR NOT NULL,
+    -- в заморозке ли операция для расследования
+    is_frozen BOOLEAN NOT NULL DEFAULT FALSE)
