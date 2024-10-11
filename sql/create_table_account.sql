aad86570575e363667c790bb2c003652c51c8435
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-11 14:13:35 +0500

    Поправил ошибки в sql запросе

--- sql/create_table_account.sql
+++ sql/create_table_account.sql
@@ -1,15 +1,15 @@
 -- Создает талицу со счетами клиентов банка
-CREATE TABLE IF NOT EXIST account_kompass(
+CREATE TABLE IF NOT EXISTS account_kompass(
     account_id SERIAL PRIMARY KEY,
-    client_id integer REFERENCES table_users(client_id),
-    accoutn_number VARCHAR NOT NULL,
+    client_id INTEGER REFERENCES table_users(client_id),
+    account_number VARCHAR NOT NULL,
     -- валюту счета нажно забирать из другой таблицы
     account_currency VARCHAR NOT NULL,
-    -- 10 это обая точность (presition)
+    -- 10 это обая точность (precision)
     -- 2 это масштаб (scale)
-    account_balance DECIMAL(10:2) DEFAULT 0,
+    account_balance DECIMAL(10,2) DEFAULT 0,
     -- Данные account_type  нужно забирать из другой таблицы
     -- что бы нельзя было накосячить и создать счет с непонятным типом
     account_type VARCHAR NOT NULL,
-    account_created_date: TIMESTAMP NOT NULL,
-    account_cloused_date: TIMESTAMP);
+    account_created_date TIMESTAMP NOT NULL,
+    account_closed_date TIMESTAMP);
