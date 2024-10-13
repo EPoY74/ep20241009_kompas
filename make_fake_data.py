aaaf737d416077039788e2f866688b9f9a365ef0
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-13 12:23:50 +0500

    Генерирую дату рождения user в заданно диапазоне. Строку не берет - нужно преобразовывать явно

--- make_fake_data.py
+++ make_fake_data.py
@@ -1,6 +1,7 @@
 """Генерируем фейковые данные для 
 БД в тетовом задании
 """
+from datetime import date
 
 from faker import Faker
 
@@ -9,13 +10,22 @@ def main():
     """
     Основной код программы.
     """
-    fake = Faker('ru_Ru')   
+    fake = Faker('ru_Ru')
+
+    start_date = date(1997, 1, 1)
+    end_date = date(2010, 10, 1)
+
+
 
     print(fake.name())
     print(fake.ascii_email())
     print(fake.date_between(start_date='-5y'))
     print(fake.phone_number())
-
-
+    
+    # Когда передаешь строку - не может почему-то распарсить
+    # Приходится действовать явно через конструктор date
+    # Это ошибка. Надо зафиксить.
+    print(fake.date_between_dates(date_start=start_date, date_end=end_date))
+    
 if __name__ == "__main__":
     main()
