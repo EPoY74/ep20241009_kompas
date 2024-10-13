b3e46b94df18e2d06049b947ea196203d3f3cb98
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-13 08:16:41 +0500

    Подготовил обязательные данные для  таблицы users

--- make_fake_data.py
+++ make_fake_data.py
@@ -9,9 +9,12 @@ def main():
     """
     Основной код программы.
     """
-    fake = Faker('ru_Ru')
+    fake = Faker('ru_Ru')   
 
     print(fake.name())
+    print(fake.ascii_email())
+    print(fake.date_between(start_date='-5y'))
+    print(fake.phone_number())
 
 
 if __name__ == "__main__":
