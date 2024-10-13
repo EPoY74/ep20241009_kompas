aba8e0639beb73b86a6d912ce3a9077eaa6406bd
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-13 21:17:11 +0500

    Поправил запрос на добавление данных: Добавил поля email и birthday

--- sql/add_users_data.sql
+++ sql/add_users_data.sql
@@ -1,4 +1,4 @@
 --Добавляет NOT NULL данные в таблицу users_kompass
 
-INSERT INTO users_kompass (first_name, account_open_day, phone_number)
-VALUES (?, ?, ?)
+INSERT INTO users_kompass (first_name, birthday, account_open_day, email, phone_number)
+VALUES (?, ?, ?, ?, ?)
