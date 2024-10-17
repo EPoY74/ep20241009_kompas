873bf2c4e689500bd3c1e59f632ae14299e9f847
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-17 18:00:30 +0500

    Исправил ошибку при использовании INTO и убрал назавние таблицы из названия столбцов

--- sql/add account_data.sql
+++ sql/add account_data.sql
@@ -1,10 +1,11 @@
 -- Active: 1728491536018@@127.0.0.1@5432@Kompass-db@public
 -- Наполняю таблицу account данными
 
-INSERT IN account_compass (account_compass.client_id,
- account_compass.account_number,
- account_compass.account_currency,
- account_compass.account_balance,
- account_compass.account_type,
- account_compass.account_created_date)
-VALUES (%s %s %s %s %s %s)
+спраINSERT INTO account_compass
+(client_id,
+ account_number,
+ account_currency,
+ account_balance,
+ account_type,
+ account_created_date)
+VALUES (%s, %s, %s, %s, %s, %s)
