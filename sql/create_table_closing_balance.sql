55d4d138fbcebdb547743b0881bc42fb366954a6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-18 09:39:56 +0500

    РЕФАКТОРИНГ: Добавил комментарии к sql запросу

--- sql/create_table_closing_balance.sql
+++ sql/create_table_closing_balance.sql
@@ -1,7 +1,9 @@
 -- Создает таблицу с остатком на конец дня полте закрытия банковского дня
 -- Active: 1728491536018@@127.0.0.1@5432@Kompass-db
 CREATE TABLE IF NOT EXISTS closing_balance(
+    -- Клиент понатно - он должен быть один
     client_id INTEGER REFERENCES users_compass(client_id),
+    -- у клиента м.б. несколько счетов - поэтому требуется account_id
     account_id INTEGER REFERENCES account_kompass(account_id),
     balance_at_day TIMESTAMP NOT NULL,
     closing_balance DECIMAL(10,2) DEFAULT 0)
