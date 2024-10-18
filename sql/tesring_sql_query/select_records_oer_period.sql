6146ca0b8984ff21eab2a0af12b348e001aae5ed
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-18 10:15:15 +0500

    UPD: Убрал коммит всех временных файлов

--- sql/tesring_sql_query/select_records_oer_period.sql
+++ sql/tesring_sql_query/select_records_oer_period.sql
@@ -6,6 +6,7 @@ operations_compass.transaction_date,
 operations_compass.operation_description 
 FROM operations_compass 
 WHERE operations_compass.transaction_date
-BETWEEN '2020-05-26' AND '2021-06-22'
+-- по времени - работает. 
+BETWEEN '2020-05-26' AND '2021-06-23 06:05:30'
 AND operations_compass.account_id = 90  
 ORDER BY operations_compass.transaction_date ASC;
