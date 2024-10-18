489f44cf3b4761ba67a31013251856a986f9605c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-18 06:19:55 +0500

    Тестирую прогрессбары, у меня почему-то в коде они рассыпаются

--- ep_202410_spam.py
+++ ep_202410_spam.py
@@ -5,6 +5,9 @@
 Почта: p174@mail.ru
 Since: 15.10.2024 
 """
+import time
+
+from progress.bar import Bar
 
 
 def add_char_left(inner_str: str, count_char: int, add_char: chr) -> str:
@@ -18,6 +21,20 @@ def add_char_left(inner_str: str, count_char: int, add_char: chr) -> str:
     out_str: str  = inner_str.rjust(count_char, add_char)
     return out_str
 
+def try_lib_progress():
+    """
+    Тестирую библиотеку progress, так как похоже
+    рассыпается от вложенного цикла
+    """
+    progress_bar = Bar('Processing', max=100)
+    for i in range(100):
+        progress_bar.next()
+        time.sleep(0.01)  # Задержка для демонстрации
+        for j in range(100):
+            time.sleep(0.01)  # Задержка для демонстрации
+            # Тут не расспается... Почему у меня сыпется в коде?
+
 
 if __name__ == "__main__":
-    print(add_char_left("Привет", 20, "0"))  
+    # print(add_char_left("Привет", 20, "0"))
+    try_lib_progress()
