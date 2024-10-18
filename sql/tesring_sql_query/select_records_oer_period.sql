2f7448e9534b473d06f16cca754121d990119a12
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-18 10:01:46 +0500

    Получение операций за период времени (по датам только)

---
+++ sql/tesring_sql_query/select_records_oer_period.sql
@@ -0,0 +1,11 @@
+-- Active: 1728491536018@@127.0.0.1@5432@Kompass-db@public
+SELECT 
+operations_compass.account_id, 
+operations_compass.amount, 
+operations_compass.transaction_date, 
+operations_compass.operation_description 
+FROM operations_compass 
+WHERE operations_compass.transaction_date
+BETWEEN '2020-05-26' AND '2021-06-22'
+AND operations_compass.account_id = 90  
+ORDER BY operations_compass.transaction_date ASC;
