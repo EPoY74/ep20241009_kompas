51a6ca6120b331c9dd27c4013398b558a05d0940
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-19 09:39:53 +0500

    Пишу функцию для подсчета операций для  таблицы закрытия дня

--- sql/function/func_calculate_sum_daily_operations.sql
+++ sql/function/func_calculate_sum_daily_operations.sql
@@ -1,1 +1,9 @@
-CREATE OR REPLACE  FUNCTION  c
+CREATE OR REPLACE  FUNCTION calculate_sum_daily_operations()
+RETURN VOID AS $$
+BEGIN
+    INSERT INTO closing_balance(
+        closing_balance.account_id,
+        closing_balance.balance_at_day
+    )
+END;
+$$ LANGUAGE PLPGSQL
