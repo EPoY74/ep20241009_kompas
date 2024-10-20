14253628c66597561027ecc4e35f55c53d2ea552
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-20 12:28:57 +0500

    Написал функцию для подсчета данных по остаток дня

--- sql/function/func_calculate_sum_daily_operations.sql
+++ sql/function/func_calculate_sum_daily_operations.sql
@@ -1,9 +1,26 @@
+-- Active: 1728491536018@@127.0.0.1@5432@Kompass-db@public
 CREATE OR REPLACE  FUNCTION calculate_sum_daily_operations()
-RETURN VOID AS $$
+RETURNS VOID AS $$
 BEGIN
     INSERT INTO closing_balance(
-        closing_balance.account_id,
-        closing_balance.balance_at_day
+        client_id,
+        account_id,
+        balance_at_day,
+        closing_balance
     )
+SELECT 
+    us.client_id AS inner_client_id,
+    ac.account_id AS inner_account_id,
+    oc.transaction_date::date AS balance_at_day,
+    SUM(oc.amount) AS closing_balance
+FROM 
+    operations_compass AS oc 
+    JOIN account_compass AS ac ON ac.account_id = oc.account_id
+    JOIN users_compass AS us ON ac.client_id = us.client_id
+GROUP BY
+    us.client_id,
+    ac.account_id,
+    oc.transaction_date::date;
+
 END;
 $$ LANGUAGE PLPGSQL
