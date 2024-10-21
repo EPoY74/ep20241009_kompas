97b5116e621d41859e07a87864d9e42188665189
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-21 12:16:43 +0500

    Проще посчитать все сначала. Запрос замудреный получился

--- sql/tesring_sql_query/now_balance.sql
+++ sql/tesring_sql_query/now_balance.sql
@@ -23,3 +23,59 @@ WHERE cb.account_id = 1
     cb.account_id
     ;
 
+--- Тестирую и ковыряюсь
+SELECT 
+    oc.transaction_date,
+    cb.client_id,
+    cb.account_id,
+    oc.amount,
+    cb.closing_balance,
+    SUM(oc.amount) AS operations_now
+FROM 
+    closing_balance AS cb
+JOIN 
+    operations_compass AS oc ON cb.account_id = oc.account_id 
+    
+WHERE cb.account_id = 1
+    -- AND cb.balance_at_day::date = (
+    --     SELECT MAX(balance_at_day::date) 
+    --     FROM closing_balance
+    --     WHERE account_id = 1
+    -- )
+ GROUP BY 
+    oc.transaction_date,
+    oc.amount,
+    cb.closing_balance,
+    cb.client_id,
+    cb.account_id
+     ORDER BY oc.transaction_date DESC
+    ;
+
+
+-- баланс на другой день
+
+SELECT 
+    cb.client_id,
+    cb.account_id,
+    cb.closing_balance,
+    SUM(oc.amount) AS operations_now
+FROM 
+    closing_balance AS cb
+LEFT JOIN 
+    operations_compass AS oc 
+    ON cb.account_id = oc.account_id 
+    
+WHERE  cb.account_id = 1 
+    AND cb.balance_at_day::date = (
+        SELECT balance_at_day::date 
+        FROM closing_balance
+        WHERE account_id = 1 AND balance_at_day::date < '2023-12-10'
+        ORDER BY balance_at_day DESC
+        LIMIT 1
+        )
+    -- AND oc.transaction_date = '2023-12-10' 
+ GROUP BY 
+    cb.client_id,
+    cb.closing_balance,
+    cb.account_id
+    ;
